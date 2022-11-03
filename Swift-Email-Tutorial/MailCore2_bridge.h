9ba67c878d79e1c635276c4e3496131888bec589
iamCodeBrah <letscodebrah@gmail.com>
2022-11-03 06:24:02 -0700

    Lesson 3 - Objective-C Header Bridge

---
+++ Swift-Email-Tutorial/MailCore2_bridge.h
@@ -0,0 +1,13 @@
+//
+//  MailCore2_bridge.h
+//  Swift-Email-Tutorial
+//
+//  Created by YouTube on 2022-11-03.
+//
+
+#ifndef MailCore2_bridge_h
+#define MailCore2_bridge_h
+
+#include "MailCore.h"
+
+#endif /* MailCore2_bridge_h */
